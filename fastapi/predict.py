b659f4d649b1d43c643d59b1aec417a10241a781
Ubuntu <ubuntu@ip-172-31-24-104.ap-south-1.compute.internal>
2021-08-05 16:38:55 +0000

    dvc pushed and compose file added

--- fastapi/predict.py
+++ fastapi/predict.py
@@ -5,7 +5,7 @@ import json
 sent_length=210
 
 x_tokenizer=XLMRobertaTokenizer.from_pretrained('tokenizer/')
-URL='http://model_server:8501/v1/models/xlm_roberta:predict'
+URL='http://model_server:8501/v1/models/xlm_model:predict'
 
 def get_predictions(text):
     test_inputs={'input_ids':np.empty((1,sent_length),np.int32),
@@ -24,10 +24,10 @@ def get_predictions(text):
     test_inputs['input_ids'][0,]=test_inputs['input_ids'][0,].tolist()
     test_inputs['attention_mask'][0,]=test_inputs['attention_mask'][0,].tolist()
     print(test_inputs)
-    #data=json.dumps({"signature_name": "serving_default",
-    # "instances": [test_inputs]})
-    #headers={"content-type": "application/json"}
-    #response=json.loads(requests.post(URL,data=data,headers=headers))['predictions']
-    #print(response)
+    data=json.dumps({"signature_name": "serving_default",
+     "instances": [test_inputs]})
+    headers={"content-type": "application/json"}
+    response=json.loads(requests.post(URL,data=data,headers=headers))['predictions']
+    return response
 
 
