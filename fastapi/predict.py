c2fe2cbf423cca4123845ec24af539d7ca22a983
Ubuntu <ubuntu@ip-172-31-24-104.ap-south-1.compute.internal>
2021-08-05 17:39:49 +0000

    correct the error

--- fastapi/predict.py
+++ fastapi/predict.py
@@ -8,9 +8,7 @@ x_tokenizer=XLMRobertaTokenizer.from_pretrained('tokenizer/')
 URL='http://model_server:8501/v1/models/xlm_model:predict'
 
 def get_predictions(text):
-    test_inputs={'input_ids':np.empty((1,sent_length),np.int32),
-                  'attention_mask':np.empty((1,sent_length),np.int32)}
-
+    test_inputs={}
     tokenized=x_tokenizer.tokenize(text)
     if len(tokenized)>sent_length-2:
         tokenized=['<s>']+tokenized[:(sent_length-2)]+['</s>']
@@ -19,10 +17,10 @@ def get_predictions(text):
         tokenized=['<s>']+tokenized+['</s>']+['<pad>']*(sent_length-curr_len-2)
     token_ids=x_tokenizer.convert_tokens_to_ids(tokenized)
     mask=np.char.not_equal('<pad>',tokenized).astype(np.int32)
-    test_inputs['input_ids'][0,]=token_ids
-    test_inputs['attention_mask'][0,]=mask
-    test_inputs['input_ids'][0,]=test_inputs['input_ids'][0,].tolist()
-    test_inputs['attention_mask'][0,]=test_inputs['attention_mask'][0,].tolist()
+    test_inputs['input_ids']=np.expand_dims(np.cast(token_ids,dtype=np.int32),axis=0)
+    test_inputs['attention_mask']=np.expand_dims(np.cast(mask,dtype=np.int32),axis=0)
+    #test_inputs['input_ids'][0,]=test_inputs['input_ids'][0,].
+    #test_inputs['attention_mask'][0,]=test_inputs['attention_mask'][0,].tolist()
     print(test_inputs)
     data=json.dumps({"signature_name": "serving_default",
      "instances": [test_inputs]})
